2cb28090a0407aa9e0c5ffaa173072c6c15bc0ea
Ilyas Git <ilyasridhuan@gmail.com>
2017-05-25 00:35:41 +0800

    Split SSS and Threshold out of Elgamal main file

--- src/ElGamal.hs
+++ src/ElGamal.hs
@@ -20,8 +20,7 @@ data PublicKey = PublicKey {
 newtype PrivateKey = PrivateKey {x :: Integer} deriving (Show)
 newtype PlainText = PlainText Integer deriving (Show,Num,Enum,Integral,Real,Ord,Eq)
 newtype CipherText = CipherText (Integer,Integer) deriving (Show,Ord,Eq,Num)
-type SplitKey = (Integer,PrivateKey)
-type Coefficients = [Double]
+
 
 instance (Num a, Num b) => Num (a,b) where
         fromInteger x = (fromInteger x, fromInteger x)
@@ -32,18 +31,10 @@ instance (Num a, Num b) => Num (a,b) where
         abs (a,b) = (abs a, abs b)
         signum (a,b) = (signum a, signum b)
 
-run :: IO ()
-run = do
-    (pub,prv) <- genKeys 16
-    threshKeys <- genThresholdKeys prv 3 5
-    ct <- standardEncrypt pub (PlainText 10)
-    let part = flip (flip partialDecrypt pub ) ct <$> threshKeys
-    print $ thresholdDecrypt pub ct part
-
 genKeys :: Int -> IO (PublicKey,PrivateKey)
 genKeys bits = do
-    q <- generateSafePrime bits
-    let p = (q - 1) `div` 2
+    p <- generateSafePrime bits
+    let q = (p - 1) `div` 2
     g <- generateBetween 1 q >>= findGenerator q
     x <- generateMax q
     let y = expSafe g x p
@@ -52,42 +43,6 @@ genKeys bits = do
     let prvKey = PrivateKey {x}
 
     return (pubKey,prvKey)
-
-partialDecrypt :: SplitKey -> PublicKey -> CipherText -> (Integer,Integer)
-partialDecrypt (i,PrivateKey{..}) PublicKey {..} (CipherText (α,β)) = (i,expSafe α x p)
-
-thresholdDecrypt :: PublicKey -> CipherText -> [(Integer,Integer)] -> Maybe PlainText
-thresholdDecrypt PublicKey{..} (CipherText (α,β)) partialDec = do
-    let coeff = prodList $ (fromIntegral . fst <$>) partialDec
-    -- let testarr = (\x -> uncurry expSafe x p) <$> zip ((fromIntegral . snd <$>) partialDec) (floor <$> coeff)
-
-    let prod = product $ zipWith (**) ((fromIntegral . snd <$>) partialDec) coeff
-    -- prodIntArr <- inverse (floor prod) p
-    return $ PlainText ( expSafe β 1 p `div` floor prod)
-
-genThresholdKeys :: PrivateKey -> Int -> Integer -> IO [SplitKey]
-genThresholdKeys PrivateKey{..} t m = do
-    polyArray <- traverse (const $ generateBetween 0 x) [1..(t-1)]
-    let polynomial = zip [1..] polyArray
-    let polyConstituents = (\y x -> y ^ fst x * snd x ) <$> [1..m] <*> polynomial
-    let chunked = chunksOf (t-1) polyConstituents
-    return $ zip [1..] $ PrivateKey . foldr (+) x <$> chunked
-
-reconstructKey :: [SplitKey] -> PrivateKey
-reconstructKey skeys = PrivateKey $ (floor . sum) lagrangeParts
-    where
-        fxs = fromIntegral. x . snd <$> skeys :: [Double]
-        prodArr = prodList $ (fromIntegral . fst <$>) skeys
-        lagrangeParts = zipWith (*) fxs prodArr
-
-prodList :: [Double] -> Coefficients
-prodList dbls = prodList' dbls (length dbls)
-
-prodList' :: [Double] -> Int -> Coefficients
-prodList' _      0 = []
-prodList' []     _ = []
-prodList' dbls@(i:is) n =
-    product ((\x  -> x / (x - i)) <$> filter (/= i) dbls) : prodList' (is ++ [i]) (n-1)
 
 modifiedEncrypt :: PublicKey -> PlainText -> IO CipherText
 modifiedEncrypt PublicKey{..} (PlainText msg) = do
