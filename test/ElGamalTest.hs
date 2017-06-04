85ca524f8c629f34a29806b02ad4ba3eece26169
Ilyas Git <ilyasridhuan@gmail.com>
2017-06-04 20:08:57 +0800

    Modified el gamal test to generate new pub/prv keys with each test

--- test/ElGamalTest.hs
+++ test/ElGamalTest.hs
@@ -5,15 +5,16 @@ import Test.QuickCheck.Monadic
 import Data.Maybe
 import ElGamal
 import Control.Monad.IO.Class
+import Components
 
 testElGamal :: IO ()
 testElGamal = do
-    bits <- generate $ abs <$> (arbitrary :: Gen Int) `suchThat` (> 10)
-    (pub,prv) <- genKeys bits
-    print pub
-    print prv
-    verboseCheckWithResult stdArgs { maxSuccess = 500 } $ prop_MultiplicativeHomomorphism pub prv
-    quickCheckWith stdArgs { maxSuccess = 500 } $ prop_EncryptDecrypt pub prv
+    -- bits <- generate $ abs <$> (arbitrary :: Gen Int) `suchThat` (> 10)
+    -- (pub,prv) <- genKeys bits
+    -- print pub
+    -- print prv
+    quickCheckWith stdArgs { maxSuccess = 5000 } $ prop_MultiplicativeHomomorphism
+    quickCheckWith stdArgs { maxSuccess = 1000 } $ newpropEncryptDecrypt
 
 
 -- monadic fromJust $
@@ -22,17 +23,28 @@ instance Arbitrary PlainText where
         pt <- (arbitrary :: Gen Integer) `suchThat` (> 0)
         return $ PlainText pt
 
+newpropEncryptDecrypt :: PlainText -> Property
+newpropEncryptDecrypt pt@(PlainText plain) =  monadicIO $ do
+    bits <- run $ generate $ abs <$> (arbitrary :: Gen Int) `suchThat` (> 10)
+    -- run $ print bits
+    (pub,prv) <- run $ genKeys bits
+    Just (PlainText p) <- run $ standardDecrypt prv pub <$> standardEncrypt pub pt
+    assert $ plain == p
+
 prop_EncryptDecrypt :: PublicKey -> PrivateKey -> PlainText -> Property
 prop_EncryptDecrypt pub prv pt@(PlainText plain) = monadicIO $ do
     -- let ct = CipherText (10,10)
-    Just (PlainText p) <- run $ decrypt prv pub <$> encrypt pub pt
+    Just (PlainText p) <- run $ standardDecrypt prv pub <$> standardEncrypt pub pt
     assert $ plain == p
 
-prop_MultiplicativeHomomorphism :: PublicKey -> PrivateKey -> PlainText -> PlainText -> Property
-prop_MultiplicativeHomomorphism pub prv pt1@(PlainText plain1) pt2@(PlainText plain2) = monadicIO $ do
-    ct <- run $ encrypt pub pt1
-    ct' <- run $ encrypt pub pt2
+prop_MultiplicativeHomomorphism :: PlainText -> PlainText -> Property
+prop_MultiplicativeHomomorphism pt1@(PlainText plain1) pt2@(PlainText plain2) = monadicIO $ do
+    bits <- run $ generate $ abs <$> (arbitrary :: Gen Int) `suchThat` (> 32)
+    -- run $ print bits
+    (pub,prv) <- run $ genKeys bits
+    ct <- run $ standardEncrypt pub pt1
+    ct' <- run $ standardEncrypt pub pt2
 
-    let ct'' = ct * ct'
-    Just (PlainText p) <- return $ decrypt prv pub ct''
+    let CipherText(α,β) = ct * ct'
+    Just (PlainText p) <- return $ standardDecrypt prv pub (CipherText (α `mod` (p pub),β `mod` (p pub)))
     assert $ p == (plain1*plain2)
