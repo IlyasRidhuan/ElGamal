af7ba6d15d8eb2f798b804d065e1a79012d195aa
Ilyas Git <ilyasridhuan@gmail.com>
2017-06-04 20:08:36 +0800

    Added single value ZKP and fixed equality of discrete logs

--- src/ZKP.hs
+++ src/ZKP.hs
@@ -12,33 +12,50 @@ interactiveProof PublicKey{..} (CipherText (α,_)) = do
     return (expSafe g τ p, expSafe α τ p,τ)
 
 challengeProof :: MonadRandom m => PublicKey -> m Integer
-challengeProof PublicKey{..} = generateMax q
+challengeProof PublicKey{..} = do
+    let maxi = floor $ logBase 2 (fromInteger q)
+    generateMax (2 ^ maxi)
 
 challengeRespond :: PrivateKey -> PublicKey -> Integer -> Integer -> Integer
 challengeRespond PrivateKey{..} PublicKey{..} τ u =
-    τ - expSafe (u * x) 1 q
+    -- τ - expSafe (u * x) 1 q
+    τ + expSafe (u * x) 1 q
 
-verifyResponse :: PublicKey -> CipherText  -> Integer -> Integer -> Integer -> Integer -> (Integer,Integer)
-verifyResponse PublicKey{..} (CipherText (α,_)) u v w z =
-    (expSafe (gw * vu) 1 p, expSafe (αw * zu) 1 p)
+verifyResponse :: PublicKey -> Integer -> Integer -> Integer -> Integer -> Integer -> (Integer,Integer)
+verifyResponse PublicKey{..} γ1 γ2 v z u =
+    (expSafe (γ1 * vu ) 1 p, expSafe (γ2 * zu) 1 p)
     where
-        gw = expSafe g w p
-        vu = expSafe v u p
-        αw = expSafe α w p
-        zu = expSafe z u p
+        vu = expFast v u p
+        zu = expFast z u p
 
-checkEqualityOfDL :: PrivateKey -> PublicKey -> (Integer, Integer) -> (Integer,Integer) -> CipherText -> IO ()
-checkEqualityOfDL prv pub vk pd ct = do
+checkEqualityOfDL :: PrivateKey -> PublicKey -> (Integer, Integer) -> (Integer,Integer) -> CipherText -> IO Bool
+checkEqualityOfDL prv pub@PublicKey{..} vk pd ct@(CipherText (α,_)) = do
     (γ1,γ2,τ) <- interactiveProof pub ct
-    print $ "tau is : " ++ show τ
-    print $ "l1 is  : " ++ (show γ1)
-    print $ "l2 is  : " ++ (show γ2)
     u <- challengeProof pub
     let w = challengeRespond prv pub τ u
     print w
-    let (γ3,γ4) = verifyResponse pub ct u (snd vk) w (snd pd)
-    print $ "l3 is  : " ++ (show γ3)
-    print $ "l4 is  : " ++ (show γ4)
-    if γ1 == γ3 && γ2 == γ4
-    then putStrLn "True"
-    else putStrLn "False"
+    let (γ3,γ4) = verifyResponse pub γ1 γ2 (snd vk) (snd pd) u
+    let gw = expFast g w p
+    let αw = expFast α w p
+    return $ gw == γ3 && αw == γ4
+    -- if gw == γ3 && αw == γ4
+    -- then putStrLn "True"
+    -- else putStrLn "False"
+
+
+
+singleInteractiveProof :: PrivateKey -> PublicKey -> IO Bool
+singleInteractiveProof PrivateKey{..} pub@PublicKey{..} = do
+    w <- generateMax q
+    let h = expFast g w p
+    r <- generateMax q
+    let a = expFast g r p
+    let t = floor $ logBase 2 (fromInteger q)
+    e <- generateMax (2 ^ t)
+    let z = r + expFast (e * w) 1 q
+    let gz = expFast g z p
+    let he = expFast h e p
+    let test = expFast (a*he) 1 p
+    print gz
+    print test
+    return $ gz == test
