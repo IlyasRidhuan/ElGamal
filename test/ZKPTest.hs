7dab74cdaf6f19dbe76a0d9bb5c1a0e32cdaf1c7
Ilyas Git <ilyasridhuan@gmail.com>
2017-06-06 21:53:50 +0800

    Updated tests to handle equality of DL

--- test/ZKPTest.hs
+++ test/ZKPTest.hs
@@ -3,35 +3,43 @@ module ZKPTest where
 import Test.QuickCheck
 import Test.QuickCheck.Monadic
 import ZKP
+import ShamirSecretSharing
+import ThresholdElGamal hiding (run)
 import Components
 import ElGamal
 import Crypto.Number.Generate
 import Crypto.Number.ModArithmetic
 
+
+instance Arbitrary PlainText where
+    arbitrary = do
+        pt <- (arbitrary :: Gen Integer) `suchThat` (> 0)
+        return $ PlainText pt
+
 testZKP :: IO ()
 testZKP = do
-    quickCheckWith stdArgs { maxSuccess = 100000 } $ prop_SingleInteractiveZKP
+    quickCheckWith stdArgs { maxSuccess = 100 } prop_SingleInteractiveZKP
+    quickCheckWith stdArgs { maxSuccess = 100 } prop_EqofDLZKP
 
 prop_SingleInteractiveZKP :: Property
 prop_SingleInteractiveZKP = monadicIO $ do
     bits <- Test.QuickCheck.Monadic.run $ generate $ abs <$> (arbitrary :: Gen Int) `suchThat` (> 10)
     (pub,prv) <- Test.QuickCheck.Monadic.run $ genKeys bits
-    let q' = (q pub)
-    let p' = (p pub)
-    let g' = (g pub)
-    let w = (q pub)
-    let h = expFast g' w p'
-    r <- run $ generateMax q'
-    let a = expFast g' r p'
-    let t = floor $ logBase 2 (fromInteger q')
-    e <- run $ generateMax (2 ^ t)
-    let z = r + expFast (e * w) 1 q'
-    let gz = expFast g' z p'
-    let he = expFast h e p'
-    let test = expFast (a*he) 1 p'
-    assert $ gz == test
+    run (sigmaProtocol prv pub) >>= assert
+    verifyZKP pub <$> run (nonInteractiveZKP prv pub)  >>= assert
 
-prop_EqofDLZKP :: Property
-prop_EqofDLZKP = monadicIO $ do
+genPos :: Gen Integer
+genPos = abs <$> (arbitrary :: Gen Integer) `suchThat` ( > 0)
+
+prop_EqofDLZKP :: PlainText -> Property
+prop_EqofDLZKP pt = monadicIO $ do
     bits <- run $ generate $ abs <$> (arbitrary :: Gen Int) `suchThat` (> 10)
     (pub,prv) <- run $ genKeys bits
+    -- ub <- run $ generate (genPos `suchThat` ( < 5))
+    -- lb <- run $ generate $ (genPos `suchThat` ( < ub))
+    threshKeys <- run $ genThresholdKeys prv 9 10
+    let verKeys = genVerificationKeys pub threshKeys
+
+    ct@(CipherText (α,β)) <- run $ standardEncrypt pub pt
+    let part = (\x -> partialDecrypt x pub ct) <$> threshKeys
+    run (checkEqualityOfDL pub ct (head threshKeys) (head verKeys) (head part)) >>= assert
