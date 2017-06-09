a9944252ac4ba9acd57733b96cf45649beab84f9
Ilyas Git <ilyasridhuan@gmail.com>
2017-06-09 15:18:36 +0800

    Checked that NIZKP for Discrete logs works on arrays.. Still need to clean up implementation

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE RecordWildCards #-}
 module Main where
 import ElGamal
 import ZKP
@@ -21,6 +20,14 @@ main = do
 
     ct@(CipherText (α,β)) <- standardEncrypt pub (PlainText 20)
     let part = (\x -> partialDecrypt x pub ct) <$> threshKeys
-    (ay,bz,z) <- nonInteractiveEqofDL pub ct (head threshKeys) (head verKeys) (head part)
-    print $ verifyZKPofDL pub ct ay bz z
+    (ay,bz,z,e) <- nonInteractiveEqofDL pub ct (head threshKeys) (head verKeys) (head part)
+    arr <- traverse (uncurry3 (nonInteractiveEqofDL pub ct)) $ zip3 threshKeys verKeys part
+    let boolArr = uncurry3 (uncurry4 (verifyZKPofDL pub ct)) <$> zip3 arr verKeys part
+    print $ verifyZKPofDL pub ct ay bz z e (head verKeys) (head part)
     print $ thresholdDecrypt pub ct (take 4 part)
+
+uncurry3 :: (a -> b -> c -> d) -> ((a,b,c) -> d)
+uncurry3 f (x,y,z)  = f x y z
+
+uncurry4 :: (a -> b -> c -> d -> e) -> ((a,b,c,d) -> e)
+uncurry4 f (w,x,y,z)  = f w x y z
