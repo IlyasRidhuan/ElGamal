82899d72317d91847e39dcab5b99d1feca1af036
Ilyas Git <ilyasridhuan@gmail.com>
2017-06-09 15:20:02 +0800

    Added more test and combined single and dl equality tests. Also added upper bound and lower bounds 'arbitrary' instances for choosing shamir secret sharing keys

--- test/ZKPTest.hs
+++ test/ZKPTest.hs
@@ -18,28 +18,29 @@ instance Arbitrary PlainText where
 
 testZKP :: IO ()
 testZKP = do
-    quickCheckWith stdArgs { maxSuccess = 100 } prop_SingleInteractiveZKP
-    quickCheckWith stdArgs { maxSuccess = 100 } prop_EqofDLZKP
+    quickCheckWith stdArgs { maxSuccess = 100 } prop_SingleNonInteractiveZKP
+    quickCheckWith stdArgs { maxSuccess = 100 } prop_EqualityOfDL
 
-prop_SingleInteractiveZKP :: Property
-prop_SingleInteractiveZKP = monadicIO $ do
+prop_SingleNonInteractiveZKP :: Property
+prop_SingleNonInteractiveZKP = monadicIO $ do
     bits <- Test.QuickCheck.Monadic.run $ generate $ abs <$> (arbitrary :: Gen Int) `suchThat` (> 10)
     (pub,prv) <- Test.QuickCheck.Monadic.run $ genKeys bits
     run (sigmaProtocol prv pub) >>= assert
     verifyZKP pub <$> run (nonInteractiveZKP prv pub)  >>= assert
 
-genPos :: Gen Integer
-genPos = abs <$> (arbitrary :: Gen Integer) `suchThat` ( > 0)
-
-prop_EqofDLZKP :: PlainText -> Property
-prop_EqofDLZKP pt = monadicIO $ do
+prop_EqualityOfDL :: PlainText -> Property
+prop_EqualityOfDL pt = monadicIO $ do
     bits <- run $ generate $ abs <$> (arbitrary :: Gen Int) `suchThat` (> 10)
     (pub,prv) <- run $ genKeys bits
-    -- ub <- run $ generate (genPos `suchThat` ( < 5))
-    -- lb <- run $ generate $ (genPos `suchThat` ( < ub))
-    threshKeys <- run $ genThresholdKeys prv 9 10
+    ub <- run $ generate ( choose (5,20) :: Gen Integer)
+    lb <- run $ generate ( choose (3,ub) :: Gen Integer)
+    threshKeys <- run $ genThresholdKeys prv lb ub
     let verKeys = genVerificationKeys pub threshKeys
-
     ct@(CipherText (α,β)) <- run $ standardEncrypt pub pt
     let part = (\x -> partialDecrypt x pub ct) <$> threshKeys
     run (checkEqualityOfDL pub ct (head threshKeys) (head verKeys) (head part)) >>= assert
+    (ay,bz,z,e) <- run $ nonInteractiveEqofDL pub ct (head threshKeys) (head verKeys) (head part)
+    assert $ verifyZKPofDL pub ct ay bz z e (head verKeys) (head part)
+
+genPos :: Gen Integer
+genPos = abs <$> (arbitrary :: Gen Integer) `suchThat` ( > 0)
