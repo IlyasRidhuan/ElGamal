5079c99899c8592cc6463975ef535d7f8bcc4424
Ilyas Git <ilyasridhuan@gmail.com>
2017-06-13 14:35:10 +0800

    Added helpful comments in main

--- app/Main.hs
+++ app/Main.hs
@@ -9,15 +9,18 @@ main :: IO ()
 main = do
     -- Generate a 64 bit key pair
     (pub,prv) <- genKeys 64
-
+    -- Generate split keys for 3 of 5 threshold encryption
     threshKeys <- genThresholdKeys prv 3 5
+    -- Calculate verification keys for the threshold keys
     let verKeys = genVerificationKeys pub threshKeys
-
+    -- Encrypt the number 20 with standard ElGamal
     ct@(CipherText (α,β)) <- standardEncrypt pub (PlainText 20)
+    -- Calculate partial decryptions with all the threshold keys
     let part = (\x -> partialDecrypt x pub ct) <$> threshKeys
-    nizkpdl <- nonInteractiveEqofDL pub ct (head threshKeys) (head verKeys) (head part)
-    print $ verifyZKPofDL pub ct nizkpdl (head verKeys) (head part)
+    -- Calculate the non interactive zero knowledge proofs associated with each decryption
     arr <- traverse (uncurry3 (nonInteractiveEqofDL pub ct)) $ zip3 threshKeys verKeys part
+    -- Verify that the NIZKP are true
     let boolArr = uncurry3 (verifyZKPofDL pub ct) <$> zip3 arr verKeys part
     print boolArr
+    -- Combine sufficient partial decryptions (4) to decrypt original cipher text (Outputs 20)
     print $ thresholdDecrypt pub ct (take 4 part)
