5a5e826714ac1e3141c088310f36409350969098
Ilyas Git <ilyasridhuan@gmail.com>
2017-12-18 17:05:35 +0800

    SSS wasn't working properly becuase double's weren't of a high enough precision (>16 decimal places for large n) also moved to modulo arithmetic to ensure SSS occurs over finite field

--- src/ShamirSecretSharing.hs
+++ src/ShamirSecretSharing.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE NamedFieldPuns             #-}
 
 module ShamirSecretSharing where
 
@@ -6,30 +7,31 @@ import Crypto.Number.Generate
 import Crypto.Number.ModArithmetic
 import Data.List.Split
 import Components (SplitKey,Coefficients,PublicKey(..),PrivateKey(..))
+import Data.Ratio
 
-genThresholdKeys :: PrivateKey -> Integer -> Integer -> IO [SplitKey]
-genThresholdKeys PrivateKey{..} t m = do
+genThresholdKeys :: PublicKey -> PrivateKey -> Integer -> Integer -> IO [SplitKey]
+genThresholdKeys PublicKey{..} PrivateKey{..} t m = do
     polyArray <- traverse (const $ generateBetween 0 x) [1..(t-1)]
     let polynomial = zip [1..] polyArray
-    let polyConstituents = (\y x -> y ^ fst x * snd x ) <$> [1..m] <*> polynomial
+    let polyConstituents = (\x y -> expSafe x (fst y) p * snd y ) <$> [1..m] <*> polynomial
     let chunked = chunksOf (fromInteger t-1) polyConstituents
     return $ zip [1..] $ PrivateKey . foldr (+) x <$> chunked
 
 reconstructKey :: [SplitKey] -> PrivateKey
-reconstructKey skeys = PrivateKey $ (floor . sum) lagrangeParts
+reconstructKey skeys = PrivateKey $ sum lagrangeParts
     where
-        fxs = fromIntegral. x . snd <$> skeys :: [Double]
+        fxs = x . snd <$> skeys
         prodArr = prodList $ (fromIntegral . fst <$>) skeys
-        lagrangeParts = zipWith (*) fxs prodArr
+        lagrangeParts = zipWith (*) fxs $ numerator <$> prodArr
 
-prodList :: [Double] -> Coefficients
+prodList :: [Integer] -> [Ratio Integer]
 prodList dbls = prodList' dbls (length dbls)
 
-prodList' :: [Double] -> Int -> Coefficients
+prodList' :: [Integer] -> Int -> [Ratio Integer]
 prodList' _      0 = []
 prodList' []     _ = []
 prodList' dbls@(i:is) n =
-    product ((\x  -> x / (x - i)) <$> filter (/= i) dbls) : prodList' (is ++ [i]) (n-1)
+    product ((\x  -> x % (x - i)) <$> filter (/= i) dbls) : prodList' (is ++ [i]) (n-1)
 
 genVerificationKeys :: PublicKey -> [SplitKey] -> [(Integer,Integer)]
 genVerificationKeys _                   []                    = []
